2bc3820138da384954542f7bb0b7fb5d2077a5bb
melissadehaan91 <80722664+melissadehaan91@users.noreply.github.com>
2021-04-24 20:09:47 +0200

    Add files via upload

---
+++ Function_Basics/script.js
@@ -0,0 +1,31 @@
+
+const makePizza = function () {
+    console.log('Make pizzadough');
+    console.log('Let the dough rest');
+    console.log('Cut your favorite ingredients in little pieces');
+    console.log('Kneed the dough into a pizza bottom');
+    console.log('Preheat the oven at 180 degrees');
+    console.log('Put tomatosauce on the pizza bottom');
+    console.log('Add your favorite ingredients');
+    console.log('Top the pizza with cheese');
+    console.log('Put the pizza in the preheated oven for 20 minutes');
+    console.log('Cut the pizza in 8 slices');
+};
+
+const makeSushi = function () {
+    console.log('Boil sushi rice');
+    console.log('Let the rice cool off');
+    console.log('Put a sheet of seaweed on the Bamboo mat');
+    console.log('Cover the seaweed with an even layer of sushi rice');
+    console.log('Smooth gently with the rice paddle');
+    console.log('Layer salmon, cream cheese, and avocado on the rice');
+    console.log('Roll it up tightly');
+    console.log('Slice the roll with a sharp knife');
+};
+
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
